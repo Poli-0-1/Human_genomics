0a65d71f421fe8f1a2bab72885607e79e1d236a5
Alberto Lupatin <55557905+AlbertoLupatin@users.noreply.github.com>
2024-06-26 15:31:12 +0200

    Add files via upload

---
+++ Recalibration/script.sh
@@ -0,0 +1,9 @@
+java -jar /Users/albertolupatin/Desktop/HumanGenomics/Labs/tools/GenomeAnalysisTK.jar -T BaseRecalibrator -R /Users/albertolupatin/Desktop/HumanGenomics/Labs/annotations/human_g1k_v37.fasta -I ../Tumor.sorted.bam -knownSites /Users/albertolupatin/Desktop/HumanGenomics/Labs/annotations/hapmap_3.3.b37.vcf -o recal.table 
+
+java -jar /Users/albertolupatin/Desktop/HumanGenomics/Labs/tools/GenomeAnalysisTK.jar -T PrintReads -R /Users/albertolupatin/Desktop/HumanGenomics/Labs/annotations/human_g1k_v37.fasta -I ../Tumor.sorted.bam -BQSR recal.table -o Tumor.sorted.recalibrated.bam
+
+java -jar /Users/albertolupatin/Desktop/HumanGenomics/Labs/tools/GenomeAnalysisTK.jar  -T BaseRecalibrator -R /Users/albertolupatin/Desktop/HumanGenomics/Labs/annotations/human_g1k_v37.fasta -I ../Tumor.sorted.bam -knownSites /Users/albertolupatin/Desktop/HumanGenomics/Labs/annotations/hapmap_3.3.b37.vcf -BQSR recal.table -o after_recal.table
+
+java -jar /Users/albertolupatin/Desktop/HumanGenomics/Labs/tools/GenomeAnalysisTK.jar -T AnalyzeCovariates -R /Users/albertolupatin/Desktop/HumanGenomics/Labs/annotations/human_g1k_v37.fasta -before recal.table -after after_recal.table -csv recal.csv -plots recal.pdf
+
+samtools view Tumor.sorted.recalibrated.bam | grep OQ | wc -l
