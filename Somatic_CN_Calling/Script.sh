bc25d7f5c35a796541a6e0f0570cd5d3a334a05e
Alberto Lupatin <55557905+AlbertoLupatin@users.noreply.github.com>
2024-06-26 16:45:23 +0200

    Update Script.sh

--- Somatic_CN_Calling/Script.sh
+++ Somatic_CN_Calling/Script.sh
@@ -1,3 +1,6 @@
 samtools mpileup -q 1 -f /Users/albertolupatin/ sktop/HumanGenomics/Labs/annotations/human_g1k_v37.fasta ../Control/Control.sorted.bam../Tumor/Tumor.sorted.bam | java-jar
 /Users/albertolupatin/Desktop/HumanGenomics/Labs/tools/VarScan.v2.3.9.jar copynumber --output-file SCNA --mpileup
-1
+
+java -jar java-jar /Users/albertolupatin/Desktop/HumanGenomics/Labs/tools/VarScan.v2.3.9.jar copyCaller SCNA.copynumber --output-file SCNA.copynumber.called
+
+Rscript CBS.R
